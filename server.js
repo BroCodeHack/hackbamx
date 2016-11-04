72d59770db09c7969f9c7ca665ec7e94eb777b15
DanielT <chakarron.super@gmail.com>
2016-11-04 17:03:38 -0600

    Ya funciona el servidor index

--- server.js
+++ server.js
@@ -1,8 +1,52 @@
-var express=require("express");
-var app=express();
-var server=require("http").Server(app)
+var express = require('express');
+var http = require('http');
+var io = require('socket.io');
+
+var app = express();
+var server = http.createServer(app);
+io = io.listen(server);
+var connections = 0;
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+app.use("/estatico",express.static('public'));
+
+var messages = [{
+	id:1,
+	text:"Hola soy un mensaaje",
+	author: "Soy daniel"
+}]
+
+io.on('connection',function (socket) {
+	console.log("se conectaron con sockets");
+	socket.emit('messages',messages);
+	socket.on('newmessage', function (data) {
+		messages.push(data);
+		io.sockets.emit('messages',messages);
+	});
+});
 
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.get('/', function(req, res){
+  res.render('index', { title: 'Morealert' });
+});
+
+io.set('log level', 1);
+
+io.sockets.on('connection', function (socket) {
+	connections++;
+  	console.log('connected',connections);
+
+  	socket.emit('server', {connections:connections});
+
+  	socket.on('disconnect', function() {
+  		connections--;
+    	console.log('Se desconecto',connections);
+  	});
+
+});
+
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
