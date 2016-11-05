9d23f74cf19e12dd23a3d558ee6ccf40140ba93e
rodrigo <rodrigoreyes658@gmail.com>
2016-11-04 22:01:39 -0600

    prueba de recepcion de datos de formularios

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var http = require('http');
 var io = require('socket.io');
-
+var bodyParser=require("body-parser");
+//******tablas*****
+//checar el nombre de la tabla y la ubicacion
+// var solicitudvoluntario=require("./models/solicitudvoluntario").solicitudvoluntario;
+// var solicitudcentro=require("./models/solicitudcentro").solicitudcentro;
+// var solicitudcomedor=require("./models/solicitudcomedor").solicitudcomedor;
+//*******************+
 var app = express();
 var server = http.createServer(app);
 io = io.listen(server);
@@ -24,6 +30,93 @@ io.on('connection',function (socket) {
 		messages.push(data);
 		io.sockets.emit('messages',messages);
 	});
+});
+
+
+
+app.get("/solicitud_comedor",function(req,res)
+{
+  var sol_comedor= //new solicitud_comedor(
+  {
+    nombre_c:req.query.nombre_c,//nombre del comedor
+    ubicacion:req.query.ubicacion,//coordenadas
+    horario:req.query.horario,
+    direccion:req.query.direccion_c,
+    ///////////////////
+    nombre:req.query.nombre,
+    direccion:req.query.direccion,
+    telefono:req.query.telefono,
+    email:req.query.email
+  }
+
+  console.log(JSON.stringify(req.query.nombre_c));
+  // sol_comedor.save().then(function(us)//guardamos en la base de datos
+  // {
+  //   console.log("Los datos se han guardado correctamente");
+  // },function(err)
+  //   {
+  //     if(err)
+  //     {
+  //       console.log(String(err));
+  //       console.log("Error al guardar los datos");
+  //     }
+  //   }
+});
+
+app.get("/solicitud_centro",function(req,res)
+{
+  var sol_centro= //new solicitud_centro(
+  {
+    nombre_a:req.query.nombre_a,//nombre del comedor
+    ubicacion:req.query.ubicacion,//coordenadas
+    horario:req.query.horario,
+    direccion_a:req.query.direccion_a,
+    ///////////////////
+    nombre:req.query.nombre,
+    direccion:req.query.direccion,
+    telefono:req.query.telefono,
+    email:req.query.email
+  }
+
+  console.log(JSON.stringify(req.query));
+  var status=true;
+  res.header("Access-Control-Allow-Origin","*");
+  res.send({"status":status});
+
+  // sol_centro.save().then(function(us)//guardamos en la base de datos
+  // {
+  //   console.log("Los datos se han guardado correctamente");
+  // },function(err)
+  //   {
+  //     if(err)
+  //     {
+  //       console.log(String(err));
+  //       console.log("Error al guardar los datos");
+  //     }
+  //   }
+});
+
+app.get("/solicitud_voluntario",function(req,res)
+{
+  var sol_voluntario= new solicitud_voluntario(
+  {
+    email:req.query.email,
+    nombre:req.query.nombre,
+    direccion:req.query.direccion,
+    telefono:req.telefono.telefono
+  });
+  console.log(req.query.nombre);
+  // sol_voluntario.save().then(function(us)//guardamos en la base de datos
+  // {
+  //   console.log("Los datos se han guardado correctamente");
+  // },function(err)
+  //   {
+  //     if(err)
+  //     {
+  //       console.log(String(err));
+  //       console.log("Error al guardar los datos");
+  //     }
+  //   }
 });
 
 
